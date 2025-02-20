fb38b0d9552410496c9bbb3db4141e669a1fcf47
Levi Siebra <levils114@gmail.com>
2025-02-20 17:36:59 -0300

    refactor: applying kiss principles in code

--- src/utils/isLightColor.ts
+++ src/utils/isLightColor.ts
@@ -1,10 +1,14 @@
 export function isLightColor(color: string): boolean {
-  var red = parseInt(color.substring(1, 3), 16);
-  var green = parseInt(color.substring(3, 5), 16);
-  var blue = parseInt(color.substring(5, 7), 16);
+  const redHexColor = color.substring(1, 3);
+  const greenHexColor = color.substring(3, 5);
+  const blueHexColor = color.substring(5, 7);
 
-  const brightness = ((red * 299) + (green * 587) + (blue * 114)) / 1000;
-  const isLightColor = brightness > 155;
+  var redColorInNumber = parseInt(redHexColor, 16);
+  var greenColorInNumber = parseInt(greenHexColor, 16);
+  var blueColorInNumber = parseInt(blueHexColor, 16);
 
+  const brightness = (redColorInNumber + greenColorInNumber + blueColorInNumber) / 710;
+
+  const isLightColor = brightness > 0.5;
   return isLightColor;
 }
